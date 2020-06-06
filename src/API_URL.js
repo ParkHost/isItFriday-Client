a89a78184e1692e9d77440c9223e515c0cef54af
ParkHost <ruud.voorjans@parkhost.eu>
2020-06-06 22:19:45 +0200

    :bug: URL change

--- src/API_URL.js
+++ src/API_URL.js
@@ -1,3 +1,3 @@
-const API_URL = window.location.hostname === 'localhost' ? 'http://localhost:5000' : 'http://isitfriday.parkhost.eu';
+const API_URL = window.location.hostname === 'localhost' ? 'http://localhost:5000' : 'https://isitfriday.parkhost.eu';
 
 export default API_URL;
